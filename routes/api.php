e1955bd1517f9b87e6f67acdc44d6a05b44d2762
Muqsithu Nafiu <nafiumndrr@gmail.com>
2024-04-08 04:44:53 +0700

    update docker yml to using mysql; install api and laravel sanctum; create auth routes;

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::prefix('/auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('/auth')->group(function () {
+        Route::get('', [AuthController::class, 'checkToken']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
+});
